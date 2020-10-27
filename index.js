f32878702b611b51554a628ddb5022549e2194e6
Zoran Terhes <zterhes1@gmail.com>
2020-10-27 16:16:23 +0100

    játékos mozgás hozzáadás

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ const player = {
 };
 
 map.drawMap(map.fillMap(map.generateMap(height, width), player));
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf8');
+stdin.on('data', (key) => {
+  console.clear();
+
+  if (key === 'd') {
+    if (player.pos.y < width - 2) {
+      player.pos.y++;
+    }
+  }
+  if (key === 'a') {
+    if (player.pos.y > 1) {
+      player.pos.y--;
+    }
+  }
+
+  if (key === 'q') {
+    process.exit(0);
+  }
+  console.clear();
+  map.drawMap(map.fillMap(map.generateMap(height, width), player));
+});
