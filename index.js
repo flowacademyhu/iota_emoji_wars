a4ef4fc22daf753b77c259ca2ee652cc136b51cb
Zoran Terhes <zterhes1@gmail.com>
2020-10-28 15:35:06 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const map = require('./map');
+
+// Pálya méretei
+const height = 20;
+const width = 50;
+
+const player = {
+  pos: {
+    x: height - 1,
+    y: Math.floor(width / 2)
+  }
+};
+const enemy = [];
+
+const enemyNum = 5;
+
+const main = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.resume();
+  // enemy generalás
+  setInterval(() => {
+    map.generateEnemy(width, enemy, enemyNum);
+  }, 100);
+  console.log(enemy);
+  // setInterval(() => {
+  //   console.clear();
+  //   map.drawMap(height, width, player, enemy);
+  // }, 200);
+
+  stdin.setEncoding('utf8');
+  stdin.on('data', (key) => {
+    if (key === 'd') {
+      if (player.pos.y < width - 2) {
+        player.pos.y++;
+      }
+    }
+    if (key === 'a') {
+      if (player.pos.y > 1) {
+        player.pos.y--;
+      }
+    }
+
+    if (key === 'q') {
+      process.exit(0);
+    }
+    console.log('x: ', player.pos.x, 'y: ', player.pos.y);
+  });
+};
+
+main();
---
+++ map.js
@@ -0,0 +1,61 @@
+const { table, getBorderCharacters } = require('table');
+
+const generateMap = (height, width) => {
+  const arr = new Array(height);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(width);
+  }
+  return arr;
+};
+
+const fillMap = (src) => {
+  for (let sor = 0; sor < src.length; sor++) {
+    for (let oszlop = 0; oszlop < src[sor].length; oszlop++) {
+      src[sor][oszlop] = ' ';
+      if (oszlop === 0 || oszlop === src[sor].length - 1) {
+        src[sor][oszlop] = 'X';
+      }
+    }
+  } return src;
+};
+
+const generateEnemy = (width, enemyArr, n) => {
+  for (let i = 0; i < n; i++) {
+    enemyArr.push({ x: Math.floor(Math.random) * width - 2 + 1, y: 0 });
+  }
+};
+
+const drawMap = (height, width, player, enemyArr) => {
+  const terkep = fillMap(generateMap(height, width));
+  for (let sor = 0; sor < terkep.length; sor++) {
+    for (let oszlop = 0; oszlop < terkep[sor].length; oszlop++) {
+      if (player.pos.x === sor && player.pos.y === oszlop) {
+        terkep[sor][oszlop] = 'P';
+      }
+      for (let i = 0; i < enemyArr.length; i++) {
+        if (enemyArr[i].pos.x === sor && enemyArr[i].pos.y === oszlop) {
+          terkep[sor][oszlop] = 'E';
+        }
+      }
+    }
+  }
+  const output = table(terkep, {
+    border: getBorderCharacters('void'),
+    columnDefault: {
+      paddingLeft: 0,
+      paddingRight: 0
+    },
+    drawHorizontalLine: () => {
+      return false;
+    }
+  });
+
+  console.log(output);
+};
+
+module.exports = {
+  generateMap,
+  fillMap,
+  drawMap,
+  generateEnemy
+};
