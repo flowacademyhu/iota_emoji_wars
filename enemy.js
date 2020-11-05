b1d14344ab001799f256c23fbe31e61e355b76d7
kerdotamas <kerdotamas83@gmail.com>
2020-11-05 15:42:47 +0100

    enemy mozog ide-oda

--- enemy.js
+++ enemy.js
@@ -1,14 +1,21 @@
 const move = require('./move');
 
-const stepEnemy = (enemy, height) => {
+const stepEnemy = (enemy, height, width) => {
   for (let i = 0; i < enemy.length; i++) {
     move.moveDown(enemy[i], height);
+    if (enemy[i].z === 0) {
+      move.moveRight(enemy[i], width)
+      enemy[i].z = 1
+    } else {
+      move.moveLeft(enemy[i])
+      enemy[i].z = 0
+    }
   }
 };
 
 const generateEnemy = (width, enemy, n) => {
   for (let i = 0; i < n; i++) {
-    enemy.push({ x: Math.floor(Math.random() * (width - 2) + 1), y: 0 });
+    enemy.push({ x: Math.floor(Math.random() * (width - 3) + 1), y: 1 , z: 0 });
   }
 };
 
